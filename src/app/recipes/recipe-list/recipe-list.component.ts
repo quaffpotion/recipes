2dc3e4887ad1a4ffe7ec42eee4610a32dc08d35b
quaffpotion <43005901+quaffpotion@users.noreply.github.com>
2019-06-01 19:34:05 -0500

    3-51

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipes.model'
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'so yummy', 'https://img.sndimg.com/food/image/upload/q_92,fl_progressive/v1/img/recipes/51/57/4/C3z30sZYS8C64frGOcGw_garlic-shrimp-and-asparagus-1.jpg')
+  ]
 
   constructor() { }
 
