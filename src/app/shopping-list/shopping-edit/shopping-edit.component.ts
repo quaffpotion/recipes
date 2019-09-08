3059179589510437a7745f7e0ed9b34396fa43e5
quaffpotion <43005901+quaffpotion@users.noreply.github.com>
2019-09-07 22:00:02 -0500

    7-88

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -15,8 +15,8 @@ import { Ingredient } from '../../shared/ingredient.model';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
-  @ViewChild('nameInput', { static: false }) nameInputReference: ElementRef;
-  @ViewChild('amountInput', { static: false }) amountInputReference: ElementRef;
+  @ViewChild('nameInput') nameInputReference: ElementRef;
+  @ViewChild('amountInput') amountInputReference: ElementRef;
   @Output() ingredientAdded = new EventEmitter<Ingredient>();
 
   constructor() {}
